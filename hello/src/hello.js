bb6ff4612d3ceb1fc7cedf6710475fe3cea32b3b
RavingAPD <raver13372@gmail.com>
2015-09-24 10:41:56 +0200

    Added hello-name.html

--- hello/src/hello.js
+++ hello/src/hello.js
@@ -1,7 +1,28 @@
 var r = React;
 
+
+var HelloName = r.createClass({
+    render: function() {
+        return (
+            <h1>Hello, { this.props.name }!</h1>
+        )
+    }
+})
+
 //Render header tag and append to hello element
-r.render(
-    <h1>Hello React</h1>,
-    document.getElementById("hello")
-);
+//hello.html
+
+// r.render(
+//     <h1>Hello React</h1>,
+//     document.getElementById("hello")
+// );
+
+//----------------------------------------
+
+//Get the HelloName module, give it a name, render it
+//hello-name.html
+
+// r.render(
+//     <HelloName name="Mark Z." />,
+//     document.getElementById("hello-name")
+// );

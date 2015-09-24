9284a97e8994acaf36d6d114ad254a5f5fe5bbb5
RavingAPD <raver13372@gmail.com>
2015-09-24 11:01:41 +0200

    Added hello-languge.html
    Also added a guide in hello.js on how to find the corresponding render
    function for the html pages

--- hello/src/hello.js
+++ hello/src/hello.js
@@ -7,7 +7,29 @@ var HelloName = r.createClass({
             <h1>Hello, { this.props.name }!</h1>
         )
     }
-})
+});
+
+var RandomHello = r.createClass({
+    render: function() {
+        var hellos = [ "Hello", "Salut", "Hej" ];
+        var rdm_min = 0;
+        var rdm_max = hellos.length;
+        var rdm = Math.floor( Math.random() * (rdm_max - rdm_min) ) + rdm_min;
+
+        return(
+            <h1>{ hellos[rdm] }, { this.props.name }!</h1>
+        )
+    }
+});
+
+//======================
+// Choose a html page
+// Remove the comments from the corresponding page
+// Eg.
+// hello-name.html
+// Find hello-name.html in this file
+// Remove comments from the render class
+//======================
 
 //Render header tag and append to hello element
 //hello.html
@@ -25,4 +47,14 @@ var HelloName = r.createClass({
 // r.render(
 //     <HelloName name="Mark Z." />,
 //     document.getElementById("hello-name")
+// );
+
+//----------------------------------------
+
+//Render "Hello { name }" in different language
+//hello-language.html
+
+// r.render(
+//     <RandomHello name="Mark Z." />,
+//     document.getElementById("hello-language")
 // );

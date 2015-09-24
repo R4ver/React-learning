8b4b157a30165ea3dda557330760feb9e9ad9a98
RavingAPD <raver13372@gmail.com>
2015-09-24 13:38:10 +0200

    Added new element: "Hello"
    See readme for instructions

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ root/
 3. Still in command prompt or terminal, navigate to your project root folder
 4. Type in ```jsx --watch /src /build```
 5. This will keep watching you src folder and compile files when changed and put them into a new folder called "build"
+
+## Usage
+```jsx
+//Include /build/hello.js in footer or bottom of page
+React.render(
+    <Hello random="true" name="Mark Z." />, //random="true" prints out "Hello" in multiple languages
+    document.getElementById("hello")
+);
+```
--- hello/src/hello.js
+++ hello/src/hello.js
@@ -1,6 +1,5 @@
 var r = React;
 
-
 var HelloName = r.createClass({
     render: function() {
         return (
@@ -16,45 +15,18 @@ var RandomHello = r.createClass({
         var rdm_max = hellos.length;
         var rdm = Math.floor( Math.random() * rdm_max ) + rdm_min;
 
-        return(
+        return (
             <h1>{ hellos[rdm] }, { this.props.name }!</h1>
         )
     }
 });
 
-//======================
-// Choose a html page
-// Remove the comments from the corresponding page
-// Eg.
-// hello-name.html
-// Find hello-name.html in this file
-// Remove comments from the render class
-//======================
-
-//Render header tag and append to hello element
-//hello.html
-
-// r.render(
-//     <h1>Hello React</h1>,
-//     document.getElementById("hello")
-// );
-
-//----------------------------------------
-
-//Get the HelloName module, give it a name, render it
-//hello-name.html
-
-// r.render(
-//     <HelloName name="Mark Z." />,
-//     document.getElementById("hello-name")
-// );
-
-//----------------------------------------
-
-//Render "Hello { name }" in different language
-//hello-language.html
-
-// r.render(
-//     <RandomHello name="Mark Z." />,
-//     document.getElementById("hello-language")
-// );
+var Hello = r.createClass({
+    render: function() {
+        if ( this.props.random === "true" ) {
+            return( <RandomHello name={ this.props.name } /> )
+        } else if ( this.props.random === "false" ) {
+            return( <HelloName name={ this.props.name } /> )
+        }
+    }
+});
